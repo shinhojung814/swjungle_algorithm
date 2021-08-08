8958a52a5d544bea1c941dfbdfbeeb16cee19766
shinhojung814 <shinhojung814@gmail.com>
2021-08-08 22:58:11 +0900

    4344. 평균은 넘겠지

---
+++ basic_1-13.py
@@ -0,0 +1,19 @@
+# 4344. 평균은 넘겠지
+# https://www.acmicpc.net/problem/4344
+
+import sys
+sys.stdin = open("basic_1-13.txt", "r")
+
+for num in range(int(input())):
+    input_arr = list(map(int, input().split()))
+    N, scores = input_arr[0], input_arr[1:]
+    
+    avg_score = sum(scores) / N
+    good = 0
+    
+    for student in scores:
+        if student > avg_score:
+            good += 1
+    
+    answer = good / N * 100
+    print(str("%.3f"%answer) + '%')
