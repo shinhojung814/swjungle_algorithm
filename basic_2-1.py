0e29db785a91212b72e9901408042182a8da2f2d
shinhojung814 <shinhojung814@gmail.com>
2021-08-08 22:59:50 +0900

    10872. 팩토리얼

---
+++ basic_2-1.py
@@ -0,0 +1,16 @@
+# 10872. 팩토리얼
+# https://www.acmicpc.net/problem/10872
+
+import math
+
+N = int(input())
+        
+print(math.factorial(N))
+
+# def factorial(N):
+#     if N > 0:
+#         return N * factorial(N - 1)
+#     else:
+#         return 1
+
+# print(factorial(N = int(input())))
