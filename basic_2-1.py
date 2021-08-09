749ef9ace7ec8730d79892c685cb6451740f4564
shinhojung814 <shinhojung814@gmail.com>
2021-08-09 10:05:33 +0900

    10872. 팩토리얼

--- basic_2-1.py
+++ basic_2-1.py
@@ -3,14 +3,10 @@
 
 import math
 
-N = int(input())
-        
-print(math.factorial(N))
+def factorial(N):
+    if N > 0:
+        return N * factorial(N - 1)
+    else:
+        return 1
 
-# def factorial(N):
-#     if N > 0:
-#         return N * factorial(N - 1)
-#     else:
-#         return 1
-
-# print(factorial(N = int(input())))
+print(factorial(N = int(input())))
