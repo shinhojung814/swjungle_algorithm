a2691a4247d494fe36124a982c71d81f59ae2ea6
shinhojung814 <shinhojung814@gmail.com>
2021-08-10 10:28:04 +0900

    2908. 상수

--- 1-19_2908.py
+++ 1-19_2908.py
@@ -1,25 +1,11 @@
 # 2908. 상수
 # https://www.acmicpc.net/problem/2908
 
-A, B = map(int, input().split())
-num1, num2 = 0, 0
+import sys
 
-while A > 0:
-    temp1 = A % 10
-    num1 = num1 * 10 + temp1
-    A = A // 10
+A, B = sys.stdin.readline().split()
 
-while B > 0:
-    temp2 = B % 10
-    num2 = num2 * 10 + temp2
-    B = B // 10
+num1 = int(A[::-1])
+num2 = int(B[::-1])
     
 print(max(num1, num2))
-
-# 다른 풀이.
-
-# import sys
-# num1, num2 = sys.stdin.readline().split()
-# num1 = num1[::-1]
-# num2 = num2[::-1]
-# print(max(int(num1), int(num2)))
