12fc3948725e5c5b1bb472da13454b89aaad53ab
shinhojung814 <shinhojung814@gmail.com>
2021-11-01 16:26:50 +0900

    WEEK13

--- WEEK13/1149_RGB거리.py
+++ WEEK13/1149_RGB거리.py
@@ -9,8 +9,6 @@ input = sys.stdin.readline
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-# print(arr)
-
 for i in range(1, N):
     arr[i][0] = min(arr[i - 1][1], arr[i - 1][2]) + arr[i][0]
     arr[i][1] = min(arr[i - 1][0], arr[i - 1][2]) + arr[i][1]
