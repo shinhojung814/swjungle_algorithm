17fe269b9427699fb3c652e894e3da5c657fbd51
shinhojung814 <shinhojung814@gmail.com>
2021-12-14 23:50:15 +0900

    WEEK19

---
+++ WEEK19/11728_배열 합치기.py
@@ -0,0 +1,14 @@
+# 11728. 배열 합치기
+# https://www.acmicpc.net/problem/11728
+
+import sys
+
+sys.stdin = open('11728_배열 합치기.txt', 'r')
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+res = sorted(A + B)
+print(*res)
