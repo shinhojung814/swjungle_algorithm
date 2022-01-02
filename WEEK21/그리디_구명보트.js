850157e4a940bdf1a63f08bd27e4d9f34597a169
shinhojung814 <shinhojung814@gmail.com>
2022-01-02 23:50:57 +0900

    WEEK21

--- WEEK21/그리디_구명보트.js
+++ WEEK21/그리디_구명보트.js
@@ -1,4 +1,17 @@
 function solution(people, limit) {
   let answer = 0;
+  people.sort(function (a, b) {
+    return b - a;
+  });
+
+  for (let i = 0, j = people.length - 1; i <= j; i++) {
+    if (people[i] + people[j] > limit) {
+      answer++;
+    } else {
+      answer++;
+      j--;
+    }
+  }
+
   return answer;
 }
